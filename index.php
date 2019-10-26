08f9c458e5c4e5cb66db69894d0839ae5ef2163c
Omar Faruque <omar.faruque967@gmail.com>
2019-10-27 03:33:28 +0600

    homepage completed

---
+++ index.php
@@ -0,0 +1,326 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="description" content="Buy wooden doors and frames in Dhaka Bangladesh">
+  <meta name="keywords" content="buy door in bangladesh, buy doors in dhaka">
+  <title>Buy Doors and Frames in Dhaka Bangladesh | Azad & Sons</title>
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+  <!-- navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark nav-custom fixed-top py-3 d-none d-lg-block">
+    <div class="container">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
+        aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <a class="navbar-brand" href="index.html">Hidden brand</a>
+        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="products.html">Products</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- navbar -->
+
+  <!-- header -->
+  <header class="bd-example">
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active carousel-custom">
+
+          <div class="carousel-caption">
+            <h1 class="display-3 text-white mb-4">Azad &amp; Sons</h1>
+            <p class="lead text-white mb-4">High grade wooden doors and frames
+            </p>
+            <a href="#" class="btn btn-lg btn-primary">Show Products</a>
+          </div>
+        </div>
+
+        <div class="carousel-item carousel-custom">
+          <div class="carousel-caption">
+            <h1 class="display-3 text-white mb-4">Azad &amp; Sons</h1>
+            <p class="lead text-white mb-4">Affordable Price
+            </p>
+            <a href="#" class="btn btn-lg btn-primary">Call For Price</a>
+          </div>
+        </div>
+        <div class="carousel-item carousel-custom">
+
+          <div class="carousel-caption">
+            <h1 class="display-3 text-white mb-4">Azad &amp; Sons</h1>
+            <p class="lead text-white mb-4">Let's have a conversation
+            </p>
+            <a href="#" class="btn btn-lg btn-primary">Contact Us</a>
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </header>
+  <!-- header -->
+
+  <!-- main -->
+  <main>
+
+    <!-- features -->
+    <section class="py-5 text-center">
+      <div class="container">
+        <h2 class="text-primary display-4 mb-5">Features</h2>
+        <div class="row py-5">
+          <div class="col-sm-4 text-center text-sm-right">
+            <img class="img-fluid" src="./img/featured-door.jpg" alt="wooden door">
+          </div>
+          <div class="col-6 offset-2 col-sm-8 offset-sm-0 text-center text-sm-left text-primary">
+            <div class="row mb-5 mt-5 mt-sm-0">
+              <div class="col-3 text-right"><i class="fas fa-check"></i></div>
+              <div class="col-9 text-left">
+                <p class="lead">25 years guarrantee</p>
+              </div>
+            </div>
+            <div class="row mb-5">
+              <div class="col-3 text-right"><i class="fas fa-check"></i></div>
+              <div class="col-9 text-left">
+                <p class="lead">Chemical treatmented</p>
+              </div>
+            </div>
+            <div class="row mb-5">
+              <div class="col-3 text-right"><i class="fas fa-check"></i></div>
+              <div class="col-9 text-left">
+                <p class="lead">Seasoned woods</p>
+              </div>
+            </div>
+            <div class="row mb-5">
+              <div class="col-3 text-right"><i class="fas fa-check"></i></div>
+              <div class="col-9 text-left">
+                <p class="lead">An enhanced view</p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-3 text-right"><i class="fas fa-check"></i></div>
+              <div class="col-9 text-left">
+                <p class="lead">More peace and quiet</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- features -->
+
+    <!-- products -->
+    <section class="py-5 text-center bg-light">
+      <div class="container">
+        <h2 class="text-primary display-4 mb-5">Products</h2>
+        <div class="row py-5">
+          <!-- door1 -->
+          <div class="col-sm-6 col-md-3 mb-5 mb-md-0">
+            <div class="card product-card-img">
+              <a href="products.html#wood_doors"><img src="./img/product-1.jpg" class="card-img-top"
+                  alt="wooden door"></a>
+
+              <div class="card-body">
+                <p class="lead text-muted">Wooden Doors</p>
+                <a href="products.html#wood_doors" class="d-block btn btn-primary">Details</a>
+              </div>
+            </div>
+          </div>
+          <!-- door2 -->
+          <div class="col-sm-6 col-md-3 mb-5 mb-md-0">
+            <div class="card product-card-img">
+              <a href="products.html#flush_door"><img src="./img/product-2.jpg" class="card-img-top"
+                  alt="flush door"></a>
+
+              <div class="card-body">
+                <p class="lead text-muted">Wooden Doors</p>
+                <a href="products.html#flush_door" class="d-block btn btn-primary">Details</a>
+              </div>
+            </div>
+          </div>
+          <!-- door3 -->
+          <div class="col-sm-6 col-md-3 mb-5 mb-sm-0">
+            <div class="card product-card-img">
+              <a href="products.html#flush_door"><img src="./img/product-3.jpg" class="card-img-top"
+                  alt="flush door"></a>
+              <div class="card-body">
+                <p class="lead text-muted">Flush Door</p>
+                <a href="products.html#flush_door" class="d-block btn btn-primary">Details</a>
+              </div>
+            </div>
+          </div>
+          <!-- door4 -->
+          <div class="col-sm-6 col-md-3">
+            <div class="card product-card-img">
+              <a href="products.html#wood_frames"><img src="./img/product-4.jpg" class="card-img-top"
+                  alt="wooden frames"></a>
+
+              <div class="card-body">
+                <p class="lead text-muted">Wooden Frames</p>
+                <a href="products.html#wood_frames" class="d-block btn btn-primary">Details</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <a href="products.html" class="btn btn-primary">See All Products</a>
+      </div>
+    </section>
+    <!-- products -->
+
+    <!-- about -->
+    <section class="py-5 text-center">
+      <div class="container">
+        <h2 class="text-primary display-4 mb-5">About Us</h2>
+        <p class="text-muted text-left">Founded in 2002 Azad &amp; Sons is one of the largest distributors of Doors and
+          Frames in
+          Bangladesh. We
+          carry top quality wooden doors, moulded and melamine flush doors and wooden frames. Since we began, we have
+          strived to give our customers the best possible service and products at the most competitive prices.</p>
+
+        <p class="text-muted text-left">If you are looking for a stress free solution for your home door, you should
+          visit or <a class="p-dark" href="contact.php">contact us</a> at least for once.</p>
+      </div>
+    </section>
+    <!-- about -->
+
+    <!-- contact -->
+    <section id="contact" class="py-5 text-center bg-light">
+      <div class="container">
+        <h2 class="text-primary display-4 mb-5">Contact Us</h2>
+
+        <!-- contact -->
+        <div class="address mb-5">
+          <p><i class="fas fa-map-marker-alt mr-2"></i> 58, French Road, Nayabazar, Dhaka</p>
+          <p><i class="fas fa-phone mr-2"></i> <a class="p-darker" href="tel:+8801621610051">
+              +8801621610051</a></p>
+          <p><i class="fas fa-phone mr-2"></i> <a class="p-darker" href="tel:+8801621610052">
+              +8801911489474</a></p>
+        </div>
+
+        <p class="mb-5 text-dark">Or, send us a message</p>
+
+        <!-- form -->
+        <form class="text-left" method="post" action="mailer.php">
+
+          <?php
+
+            if($_GET["success"] == 1) {
+                echo "<div class=\"form-messages success\">
+                            Thank You ! Your Message Has Been Sent.
+                        </div>";
+            }
+
+            if($_GET["success"] == -1) {
+                echo "<div class=\"form-messages error\">
+                Oops ! Something Went Wrong.
+            </div>";
+            }
+
+            ?>
+
+          <div class="form-group">
+            <label class="sr-only" for="name">Your Name</label>
+            <input class="form-control" type="text" name="name" id="name" placeholder="Your Name" required>
+          </div>
+          <div class="form-group">
+            <label class="sr-only" for="email">Your Email</label>
+            <input class="form-control" type="email" name="email" id="email" placeholder="Your Email" required>
+          </div>
+          <div>
+            <label class="sr-only" for="message">Message</label>
+            <textarea class="form-control" name="message" id="message" placeholder="Message"></textarea>
+          </div>
+          <input type="submit" value="Send" class="btn btn-primary mt-3">
+        </form>
+      </div>
+    </section>
+    <!-- contact -->
+
+  </main>
+  <!-- main -->
+
+  <!-- footer -->
+  <footer class="py-5 bg-dark text-white">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8 text-center mb-3 mb-sm-0">
+          <p class="small">Copyright &copy; <span id="copyText"></span>, Azad &amp; Sons. All rights reserved.</p>
+        </div>
+        <div class="col-sm-4 text-center">
+          <p class="h4">Follow us on:</p>
+          <a class="text-white" href="https://www.facebook.com/azadandsons/" target="_blank"><i
+              class="fab fa-facebook-square"></i></a>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- footer -->
+
+  <!-- scripts -->
+  <script src="https://kit.fontawesome.com/024f9e481d.js" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+    crossorigin="anonymous"></script>
+  <script src="./js/bootstrap.min.js"></script>
+
+  <script>
+    $(document).ready(function () {
+      // date
+      $('#copyText').text(new Date().getFullYear());
+
+      // navbar
+      $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > 300) {
+          $(".navbar").addClass("bg-white");
+          $(".navbar").addClass("navbar-light");
+          $(".navbar").addClass("shadow");
+          $(".navbar").removeClass("navbar-dark");
+          $(".navbar").addClass("d-block");
+          $(".navbar").removeClass("d-lg-block");
+        } else {
+          $(".navbar").addClass("navbar-dark");
+          $(".navbar").removeClass("bg-white");
+          $(".navbar").removeClass("navbar-light");
+          $(".navbar").removeClass("shadow");
+          $(".navbar").removeClass("d-block");
+          $(".navbar").addClass("d-lg-block");
+        }
+      })
+    })
+  </script>
+  <!-- scripts -->
+</body>
+
+</html>
